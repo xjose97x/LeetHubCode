8019c43e07f7feb7601c9a9198cd98755bb737f2
José I. Escudero <joseignacioescudero97@hotmail.com>
2021-09-05 13:45:48 -0500

    Time: 104 ms (55.78%), Space: 17.6 MB (58.84%) - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        values = {}
+        for s in strs:
+            sorted_str = str(sorted(s))
+            if not sorted_str in values:
+                values[sorted_str] = []
+            values[sorted_str].append(s)
+        return list(values.values())
