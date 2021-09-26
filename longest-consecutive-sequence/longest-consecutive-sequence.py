10609a1d04ee06f4caa2ba1e788b9e2a0ae263ab
José I. Escudero <joseignacioescudero@gmail.com>
2021-09-25 21:30:18 -0500

    Time: 1904 ms (22.61%), Space: 26 MB (30.92%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.py
+++ longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -2,22 +2,13 @@ from queue import PriorityQueue
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        heap = PriorityQueue()
-        nums = set(nums)
+        numSet = set(nums)
+        result = 0
         for n in nums:
-            heap.put(n)
+            if n - 1 not in numSet:
+                length = 0
+                while n + length in numSet:
+                    length += 1
+                result = max(result, length)
         
-        result = 1
-        runner = 1
-        prev = heap.get()
-        while not heap.empty():
-            current = heap.get()
-            if prev + 1 == current:
-                runner += 1
-            else:
-                runner = 1
-            prev = current
-            result = max(result, runner)
         return result
